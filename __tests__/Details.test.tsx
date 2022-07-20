858732dffd56babd0d56f9d7df6b4750cfe862a4
suresha-manjunatha <suresha.bm@cognitiveclouds.com>
2022-07-20 19:09:47 +0530

    changes

--- __tests__/Details.test.tsx
+++ __tests__/Details.test.tsx
@@ -9,18 +9,19 @@ jest.mock("@react-navigation/native", () => {
   };
 });
 
-test("test for display content page", async () => {
-  jest.setTimeout(15000);
+jest.setTimeout(15000);
+test("test for display Details page", async () => {
   const tree = render(<Deatails />);
   expect(tree).toMatchSnapshot();
 
   await new Promise((r) => setTimeout(r, 10000));
-  // const test = screen.getByTestId("test-0");
-  // expect(test).toBeTruthy();
-  // const btn2 = screen.getByTestId("capital");
-  // fireEvent.press(btn2);
-  // const btn3 = screen.getByTestId("back1");
-  // fireEvent.press(btn3);
+  // const test1 =
+  const test = screen.findByTestId("test-0");
+  expect(test).toBeTruthy();
+  const btn2 = await screen.getByTestId("capital");
+  fireEvent.press(btn2);
+  const btn3 = screen.getByTestId("back1");
+  fireEvent.press(btn3);
   const btn1 = screen.getByTestId("back");
   fireEvent.press(btn1);
 });
